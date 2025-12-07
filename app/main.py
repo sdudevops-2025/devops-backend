51f738530b5923c51675f87842daa6abf9ce605d
K-Abdulazym <abu777ks@gmail.com>
2025-12-07 14:48:54 +0500

    front

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 from .db import engine, Base
 from .routes import router as places_router
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI(title='QuietPlaces API')
 
 Base.metadata.create_all(bind=engine)
 
 app.include_router(places_router, prefix='')
+
+app.mount("/", StaticFiles(directory="app/static", html=True), name="static")
